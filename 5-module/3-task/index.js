5b22673e9275a76c038b76913ae306159e45122e
Zloy Morty <victorbushmin67@gmail.com>
2023-10-04 19:32:49 +0100

    :)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,45 @@
 function initCarousel() {
-  // ваш код...
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  let slide = document.querySelector('.carousel__inner');
+  let showWindow = document.querySelector('.carousel');
+  let counter = 1;
+
+  carouselArrowLeft.style.display = 'none';
+
+  function moveToRight(slide) {
+    carouselArrowLeft.style.display = 'flex';
+
+    slide.style.transform = `translateX(-${showWindow.offsetWidth * counter}px)`;
+
+    counter++;
+
+    if (counter >= 4) {
+      carouselArrowRight.style.display = 'none';
+
+      return;
+    }
+  };
+
+  function moveToLeft(slide) {
+    carouselArrowRight.style.display = 'flex';
+
+    counter--;
+
+    slide.style.transform = `translateX(-${showWindow.offsetWidth * (counter - 1)}px)`;
+
+    if (counter == 1) {
+      carouselArrowLeft.style.display = 'none';
+      
+      return;
+    }
+  };
+
+  carouselArrowRight.addEventListener('click', () => {
+    moveToRight(slide);
+  });
+
+  carouselArrowLeft.addEventListener('click', () => {
+    moveToLeft(slide);
+  });
 }
--- assets/lib/create-element.js
+++
@@ -1,6 +0,0 @@
-// doesn't work for td and some other elements that may not be placed into <div>
-export default function(html) {
-  const div = document.createElement('div');
-  div.innerHTML = html;
-  return div.firstElementChild;
-};
