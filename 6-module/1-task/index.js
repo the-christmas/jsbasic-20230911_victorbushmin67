7f22ba97658784543a382250a9389bd0fe57ed74
Zloy Morty <victorbushmin67@gmail.com>
2023-10-18 17:34:12 +0100

    :)

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,50 @@
  */
 export default class UserTable {
   constructor(rows) {
+    const table = document.createElement('table');
+    this.elem = table;
+    table.innerHTML = `
+        <thead>
+            <tr>
+                <th>Имя</th>
+                <th>Возраст</th>
+                <th>Зарплата</th>
+                <th>Город</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      `;
+    let workers = []; 
+
+    rows.map(row => {
+      this.name = row.name;
+      this.age = row.age;
+      this.salary = row.salary;
+      this.city = row.city;
+      
+      workers.push(this.#template());
+    });
+
+    workers = workers.join('');
+    table.querySelector('tbody').innerHTML = workers;
+
+    let buttons = table.querySelectorAll('button');
+    buttons.forEach(item => item.addEventListener('click', () => {
+      item.parentElement.parentElement.remove();
+    }));
+  }
+
+  #template() {
+    return `
+      <tr>
+          <td>${this.name}</td>
+          <td>${this.age}</td>
+          <td>${this.salary}</td>
+          <td>${this.city}</td>
+          <td><button>X</button></td>
+      </tr>
+    `;
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,60 @@
 export default class ProductCard {
+  elem = null;
+  
+  #name = '';
+  #price = null;
+  #category = '';
+  #image = '';
+  #id = null;
+  
   constructor(product) {
+    this.#name = product.name || this.#name;
+    this.#price = product.price || this.#price;
+    this.#category = product.category || this.#category;
+    this.#image = product.image || this.#image;
+    this.#id = product.id || this.#id;
+    
+    this.#render();
   }
+  
+  #render() {
+    this.elem = createElement(this.#template());
+
+    this.elem.addEventListener('click', this.#addToCart);
+  }
+
+  #addToCart = () => {
+    // this.elem.addEventListener('product-add', function(event) {
+    //   console.log(event);
+    // });
+
+    this.elem.dispatchEvent(new CustomEvent('product-add', {
+      detail: this.#id,
+      bubbles: true
+    }));
+  }
+  
+  #template() {
+    return `
+    <div class="card">
+      <div class="card__top">
+          <img src="/assets/images/products/${this.#image}" class="card__image" alt="product">
+          <span class="card__price">€${this.#price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${this.#name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+    </div>
+    `
+  }
+}
+
+function createElement(html) {
+  const temp = document.createElement('div');
+  temp.innerHTML = html;
+
+  return temp.firstElementChild;
 }
